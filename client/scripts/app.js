a4416305bf12cc85cab83b9aac3374aa439e5f76
wyattfry <wyattfry@gmail.com>
2018-01-27 17:21:11 -0800

    Sends and receives messages from server

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,37 +1,60 @@
 // YOUR CODE HERE:
-var app = {};
-
-app.server = 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages';
-
-app.messages = [];
-
-app.rooms = [];
+var app = {
+  server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes',
+  api: {
+    getMessages: '/messages',
+    postMessages: '/messages'
+  },
+  messages: [],
+  rooms: [],
+  friends: [],
+  currentRoom: undefined,
+  username: ''
+};
 
 app.init = function() {
-  $('.create-new-room-form').hide();
-  $('.username').on('click', this.handleUsernameClick);
-  $('#send .submit').on('submit', this.handleSubmit);
+  this.hideCreateRoomForm();
+  $('#chats').on('click', '.username', this.handleUsernameClick);
+  $('.send-new-message-form').on('submit', this.handleSubmit);
+  $('#send').on('submit', this.handleSubmit);
   $('#roomSelect').on('change', this.showRoomClick);
   $('.open-create-new-room-form').on('click', this.openCreateNewRoomFormClick);
-  $('.create-room-button').on('click', this.createRoomButtonClick);
+  $('.retrieve-messages').on('click', this.getMessagesForRoomClick);
+  $('#createRoomButton').on('click', this.createRoomButtonClick);
+  $('#cancelCreateRoomButton').on('click', this.hideCreateRoomForm);
+  this.username = this.getURLParameter('username');
 };
 
 // Messages section
-app.send = function(obj) {
+app.send = function(message) {
+  console.log(message);
   $.ajax({
-    url: this.server,
+    url: this.server + this.api.postMessages,
     type: 'POST',
-    data: obj,
+    data: JSON.stringify(message),
     contentType: 'application/json',
-    success: function() {}
+    success: function(data) {
+      console.log(data);
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
   });
 };
 
-app.fetch = function() {
+app.fetch = function(room) {
+  let options = {
+    'order': '-createdAt',
+  };
+  if (room) {
+    options.where = {'roomname': room};
+  }
+  
   $.ajax({
-    url: this.server,
+    url: this.server + this.api.getMessages,
     type: 'GET',
-    data: {order: '-createdAt'},
+    data: options,
     success: function(data) {
       console.log(data);
       data.results.forEach(msg => {
@@ -43,6 +66,7 @@ app.fetch = function() {
       app.messages = app.messages.concat(data.results);
       app.extractRooms(data.results);
       app.messages.reduceRight((acc, el) => app.renderMessage(el));
+      app.showRoomClick();
     },
     error: function(error) {
       console.log('Fetch failed:', error);
@@ -56,16 +80,26 @@ app.clearMessages = function() {
 
 // Display messages retrieved from the parse server.
 app.renderMessage = function(message) {
-  let $messageContainer = $(`<div class="messageContainer" data-roomname="${message.roomname || 'lobby'}"></div>`);
+  let $messageContainer = $(`<div class="messageContainer chat" data-roomname="${message.roomname || 'lobby'}"></div>`);
   let $user = $('<span class="username"></span>').text(message.username);
   let $messageText = $('<span class="messageText"></span>').text(message.text);
   $messageContainer.append($user);
   $messageContainer.append($messageText);
   $('#chats').append($messageContainer);
+  // $('.username').on('click', this.handleUsernameClick);
 };
 
-app.handleSubmit = function() {
-
+app.handleSubmit = function(e) {
+  e.preventDefault();
+  if (!app.isValidFormByClass('.send-new-message-form')) {
+    return;
+  }
+  app.send({
+    'roomname': $('#roomSelect').val(),
+    'text': $('#message').val(),
+    'username': app.username
+  });
+  return false;
 };
 
 // Rooms section
@@ -76,8 +110,10 @@ app.extractRooms = function(data) {
 };
 
 app.renderRoom = function(roomName) {
-  let $roomNode = $(`<option value ="${roomName}">${roomName}</option>`);
-  $('#roomSelect').append($roomNode);
+  if (!($(`#roomSelect option[value="${roomName}"]`).text() === roomName)) { 
+    let $roomNode = $(`<option value ="${roomName}">${roomName}</option>`);
+    $('#roomSelect').append($roomNode);
+  }
 };
 
 app.showRoomClick = function() {
@@ -91,9 +127,17 @@ app.openCreateNewRoomFormClick = function() {
   $('.create-new-room-form').show();
 };
 
+app.hideCreateRoomForm = function(event) {
+  $('.create-new-room-form').hide();
+  if (event) {   
+    event.preventDefault();
+    event.stopPropagation();
+  }
+};
+
 app.isValidFormByClass = function(formClass) {
   let formIsValid = true;
-  $(`${formClass} :input:visible[required="required"]`).each(function() {
+  $(`${formClass} :input:visible`).each(function() {
     if (!this.validity.valid || $(this).val() === '') {
       $(this).focus();
       formIsValid = false;
@@ -103,8 +147,9 @@ app.isValidFormByClass = function(formClass) {
   return formIsValid;
 };
 
-app.createRoomButtonClick = function() {
+app.createRoomButtonClick = function(e) {
   if (!app.isValidFormByClass('.create-new-room-form')) {
+    e.preventDefault();
     return;
   }
   let roomName = $('.room-name').val();
@@ -113,24 +158,53 @@ app.createRoomButtonClick = function() {
     return;
   }
   app.rooms.push(roomName);
-  $('.create-new-room-form').hide();
+  app.hideCreateRoomForm();
   app.renderRoom(roomName);
   $('#roomSelect').val(roomName); //selects the room
   app.clearMessages();
+  e.preventDefault();
 };
 
+app.getMessages = function() {
+  app.clearMessages();
+  app.messages = [];
+  app.fetch(app.currentRoom);
+};
+
+app.getMessagesForRoomClick = function() {
+  app.clearMessages();
+  app.messages = [];
+  app.currentRoom = $('#roomSelect').val();
+  app.fetch(app.currentRoom);
+};
+
+app.getMessagesAutomatically = function() {
+  setInterval(app.getMessages, 2000);
+};
 
 // User section
 app.handleUsernameClick = function() {
-
+  // $(this).addClass('friend');
+  let newFriendName = $(this).text();
+  if (!_.contains(app.friends, newFriendName)) {
+    app.friends.push(newFriendName);
+  }
+  $(`.username:contains("${newFriendName}")`).addClass('friend');
 };
 
-$(function() {
-  app.init();
-  app.fetch();
-});
 
-
+// Helper Functions
+// credit: http://www.jquerybyexample.net/2012/06/get-url-parameters-using-jquery.html
+app.getURLParameter = function(parameter) {
+  let url = _.escape(window.location.search.substring(1));
+  let variables = url.split('&');
+  for (let i = 0; i < variables.length; i++) {
+    let parameterName = variables[i].split('=');
+    if (parameterName[0] === parameter) {
+      return parameterName[1];
+    }
+  }
+};
 
 
 // Setup a way to refresh the displayed messages (either automatically
